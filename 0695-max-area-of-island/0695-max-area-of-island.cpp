d6eb1b92b93f6061732b9cc0a5e3419de3ac0e5d
Ashwin <112781675+Ashwin-2408@users.noreply.github.com>
2025-06-05 22:11:57 +0530

    Time: 4 ms (41.62%), Space: 28.7 MB (40.37%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,20 +1,40 @@
 class Solution {
     vector<vector<int>> visited;
+    vector<pair<int,int>> dir={{0,-1},{0,1},{1,0},{-1,0}};
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         
         int n=grid.size();
         int m=grid[0].size();
         visited.assign(n,vector<int>(m,0));
-        for(int i=0;i<m;i++){
-            dfs(0,i,grid);
+       
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1 && visited[i][j]!=1){
+                    int area=dfs(i,j,grid);
+                    ans=max(ans,area);
+                }
+            }
         }
-        for(int i=0;i<m;i++){
-            dfs(n-1,i,grid);
-        }
-        for(int i=0;i<m;i++){
-            dfs(n-1,i,grid);
-        }
+        return ans;
+    }
+    int dfs(int r,int c,vector<vector<int>> & grid){
         
+        if(grid[r][c]==0 || visited[r][c]==1){
+            return 0;
+        }
+        visited[r][c]=1;
+        grid[r][c]=0;
+        int area = 1;
+        
+        for(auto it:dir){
+            int new_x=it.first+r;
+            int new_y=it.second+c;
+            if(new_x>=0 && new_x<grid.size() && new_y>=0 && new_y<grid[0].size()){
+                area+=dfs(new_x,new_y,grid);
+            }
+        }
+        return area;
     }
 };
