4e0b74e58fcd6292d13e136007c6050f7f5ac1dc
Ashwin <112781675+Ashwin-2408@users.noreply.github.com>
2025-06-23 00:38:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3551-minimum-swaps-to-sort-by-digit-sum/3551-minimum-swaps-to-sort-by-digit-sum.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    static bool comparator(pair<int, int>& a, pair<int, int>& b) {
+        if (a.first == b.first) {
+            return a.second < b.second;
+        } else {
+            return a.first < b.first;
+        }
+    }
+    int minSwaps(vector<int>& nums) {
+        vector<pair<int, int>> arr;
+        for (auto it : nums) {
+            int dum = it;
+            int sum = 0;
+            while (it > 0) {
+                sum += it % 10;
+                it /= 10;
+            }
+            arr.push_back({sum, dum});
+        }
+        sort(arr.begin(), arr.end(), comparator);
+        for (auto it : arr) {
+            cout << it.first << " " << it.second;
+            cout << endl;
+        }
+        int swaps = 0;
+        unordered_map<int, int> mpp;
+        for (int i = 0; i < arr.size(); i++) {
+            mpp[arr[i].second] = i;
+        }
+        vector<int> pos(arr.size());
+        for (int i = 0; i < arr.size(); i++) {
+            pos[i] = mpp[nums[i]];
+        }
+        vector<bool> visited(arr.size(), false);
+        for (int i = 0; i < arr.size(); i++) {
+            if (visited[i] || pos[i] == i)
+                continue;
+
+            int cycle_size = 0;
+            int j = i;
+
+            while (!visited[j]) {
+                visited[j] = true;
+                j = mpp[nums[j]];
+                cycle_size++;
+            }
+
+            if (cycle_size > 1){
+                swaps += (cycle_size - 1);
+            }
+        }
+        return swaps;
+    }
+};
