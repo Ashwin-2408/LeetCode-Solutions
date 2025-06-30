e96561eb18f36e2781751d4826fbbaa2fbdad9ed
Ashwin <112781675+Ashwin-2408@users.noreply.github.com>
2025-06-30 23:18:05 +0530

    Time: 28 ms (32.17%), Space: 79 MB (10.13%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -2,12 +2,20 @@ class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
         vector<int> ans(n,0);
+        vector<int> diff(n+1,0);
         for(auto it:bookings){
-            for(int i=it[0]-1;i<=it[1]-1;i++){
-                ans[i]+=it[2];
-            }
+            diff[it[0]-1]+=it[2];
+            diff[it[1]]-=it[2];
+
+        
         }
-        return  ans;
+        ans[0]=diff[0];
+        for(int i=1;i<n;i++){
+            ans[i]=ans[i-1]+diff[i];
+        }
+        return ans;
+
+        
         
     }
 };
