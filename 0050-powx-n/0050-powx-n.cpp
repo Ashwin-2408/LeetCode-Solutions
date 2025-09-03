99ffcf790c6292b045f35b39e5d0e7d07cd36a00
Ashwin <112781675+Ashwin-2408@users.noreply.github.com>
2025-09-04 02:34:53 +0530

    Time: 0 ms (100%), Space: 8.6 MB (43.56%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0){
+            return 1;
+        }
+        double carry=1;
+        bool negative=false;;
+        if(n<0){
+            negative=true;
+        }
+        long n1=abs((long)n);
+        while(n1>1){
+            if(n1%2==1){
+                carry*=x;
+            }
+            x=x*x;
+            n1=n1/2;
+
+
+        
+        }
+        if(negative){
+                return 1/(x*carry);
+        }
+        else{
+            
+                return x*carry;
+            
+        }
+    }
+};
