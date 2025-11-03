2ee8159cd4f37a43f8633522da6074e7a7e56266
Ashwin <112781675+Ashwin-2408@users.noreply.github.com>
2025-11-03 22:58:30 +0530

    Time: 12 ms (64.09%), Space: 17.5 MB (17.43%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,36 +10,21 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> node1,node2;
-        find_ancestors(root,p,node1);
-        find_ancestors(root,q,node2);
-        int i=0;
-        TreeNode* ancestor;
-        while(i<node1.size() && i<node2.size()){
-            if(node1[i]==node2[i]){
-                ancestor=node1[i];
-            }
-            i++;
+        if(!root || root==p || root==q){
+            return root;
         }
-        return ancestor;
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
 
+        if(left && right){
+            return root;
+        }
+        if(left){
+            return left;
+        }
+        else{
+            return right;
+        }
         
-    }
-    bool find_ancestors(TreeNode* root,TreeNode* target,vector<TreeNode*>& ancestor){
-        if(root==nullptr){
-            return false;
-        }
-        ancestor.push_back(root);
-        if(root==target){
-            return true;
-
-        }
-        if(find_ancestors(root->left,target,ancestor)||find_ancestors(root->right,target,ancestor)){
-            return true;
-        }
-        ancestor.pop_back();
-        return false;
-        
-
     }
 };
