5cffde9ea7b4941ab3a9ecb4ae8462d87d0f7aea
Ashwin <112781675+Ashwin-2408@users.noreply.github.com>
2025-11-03 23:17:19 +0530

    Time: 0 ms (100%), Space: 15.1 MB (65.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,60 +1,34 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = nums[0];
-        int right = nums[nums.size() - 1];
-
-        if (left > right) {
-            int change = -1;
-            for (int i = 0; i < nums.size(); i++) {
-                if (nums[i] > nums[i + 1]) {
-                    change = i;
-                    break;
-                }
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
             }
-            if (target >= left) {
-                left = 0;
-                right = change;
-                while (left <= right) {
-                    int mid = left + (right - left) / 2;
-                    if (nums[mid] == target) {
-                        return mid;
-                    } else if (nums[mid] > target) {
-                        right = mid - 1;
-                    } else {
-                        left = mid + 1;
-                    }
+            if(nums[mid]<nums[high]){
+                if(target>nums[mid] && target<=nums[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
                 }
 
-            } else {
-                right = nums.size() - 1;
-                left = change + 1;
-                while (left <= right) {
-                    int mid = left + (right - left) / 2;
-                    if (nums[mid] == target) {
-                        return mid;
-                    } else if (nums[mid] > target) {
-                        right = mid - 1;
-                    } else {
-                        left = mid + 1;
-                    }
+            }
+            else{
+                if(target<nums[mid] && target>=nums[low]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
                 }
             }
-        } else {
-            left=0;
-            right=nums.size()-1;
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (nums[mid] == target) {
-                    return mid;
-                } else if (nums[mid] > target) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }
-            
         }
         return -1;
+
+
+        
     }
 };
