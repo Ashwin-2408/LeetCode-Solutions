c61813647399ac672660635d3be22c12207a4768
Ashwin <112781675+Ashwin-2408@users.noreply.github.com>
2025-12-09 18:42:47 +0530

    Time: 549 ms (77.74%), Space: 346.8 MB (75.55%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.cpp
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
-        unordered_map<int,int> suffix;
-        unordered_map<int,int> prefix;
-
-        for (auto it: nums){
-            suffix[it]++;
-            
-        }
-        int result=0;
-        int mod = 1e9+7;
+        int MOD=1e9+7;
+        unordered_map<int,int> mpp;
+        vector<long long> suffix(nums.size(),0);
+        vector<long long> prefix(nums.size(),0);
         for(int i=0;i<nums.size();i++){
-            suffix[nums[i]]--;
-            
-            int required= 2* nums[i];
-            long long count_pre=prefix[required];
-            long long count_suf=suffix[required];
-            
-            result=(result+((count_pre*count_suf)%mod))%mod;
-            prefix[nums[i]]++;
-            
+            if(mpp.count(2*nums[i])){
+                prefix[i]=mpp[2*nums[i]];
+            }
+            mpp[nums[i]]++;
+
         }
-        return result;
-        
+        mpp.clear();
+        for(int i=nums.size()-1;i>=0;i--){
+            if(mpp.count(2*nums[i])){
+                suffix[i]=mpp[2*nums[i]];
+            }
+            mpp[nums[i]]++;
+
+        }
+        long long ans=0;
+        for(int i=0;i<nums.size();i++){
+            ans=(ans+(prefix[i]*suffix[i])%MOD)%MOD;
+        }
+        return ans;
     }
 };
